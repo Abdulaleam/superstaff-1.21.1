4e3afec151bd04b6572cbb5a1360f265225ac303
Abdulaleam <167490133+Abdulaleam@users.noreply.github.com>
2026-05-27 12:54:12 +0300

    Initial commit

---
+++ src/main/java/net/rainy/superstaff/SuperstaffDataGenerator.java
@@ -0,0 +1,11 @@
+package net.rainy.superstaff;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SuperstaffDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
