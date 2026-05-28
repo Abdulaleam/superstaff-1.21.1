44e7a10d2c94d92fbfb18b7eb196342f4f37d64f
Abdulaleam <167490133+Abdulaleam@users.noreply.github.com>
2026-05-28 12:45:20 +0300

    ADDED A MOD RECIPE PROVIDER AND ADDED TWO RECIPES

--- src/main/java/net/rainy/superstaff/SuperstaffDataGenerator.java
+++ src/main/java/net/rainy/superstaff/SuperstaffDataGenerator.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.minecraft.data.client.ModelProvider;
 import net.rainy.superstaff.datagen.ModModelProvider;
+import net.rainy.superstaff.datagen.ModRecipeProvider;
 
 public class SuperstaffDataGenerator implements DataGeneratorEntrypoint {
 	@Override
@@ -11,5 +12,6 @@ public class SuperstaffDataGenerator implements DataGeneratorEntrypoint {
 		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
 		pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModRecipeProvider::new);
 	}
 }
